d4b7eab796dfda24df7a1851977dc09b1b679b84
Arturo Hernández <arturoaureliohr@gmail.com>
2023-06-08 15:55:43 -0400

    Implement error handling

--- 02-create-go-module/hello/hello.go
+++ 02-create-go-module/hello/hello.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("GladOS")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
